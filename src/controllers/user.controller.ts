b94af29f7a271091dc474e18cd5a82b694569603
Nicolas Vera <nicolasveraa97@gmail.com>
2023-10-29 22:42:56 -0300

    commit

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,20 +1,22 @@
 import { Request, Response } from "express";
+import { pool } from "../config/database-pg";
 
 type UserController = {
-    create: (req:Request, res:Response)=> any
+    getPatitoName: (req:Request, res:Response)=> any
 }
 
 const userController:UserController = {
-    
-    create:async (req:Request, res:Response) =>{
+
+    getPatitoName:async (req:Request, res:Response) =>{
 
         try{
     
+            const response = await pool.query('SELECT * FROM patitos_info WHERE name ILIKE ($1);',[req.query.name])
             // logic here
     
-            return res.status(200).send({msg:'All good'})
+            return res.status(200).send({msg:'All good', data:response.rows, search:req.query})
         } catch (error:any) {
-            return res.status(500).send({msg:'Something happened'})
+            return res.status(500).send({msg:'Something happened', error})
         }
     }
 }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,6 +3,6 @@ import { userController } from "../controllers/user.controller";
 
 const router = Router()
 
-router.get('/', userController.create)
+router.get('/', userController.getPatitoName)
 
 export { router }
